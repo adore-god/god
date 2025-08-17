c5f796ef46b4b0f793898b8765ad73158af90e97
Adorations <mrs.kelly.uk@gmail.com>
2025-08-17 09:50:21 +0100

    Incremental sync (ignore images)

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-(function initBLBTagger() {
-    if (window.BLB && BLB.Tagger) {
-        // Apply your settings
-        BLB.Tagger.Translation = 'BBE';
-        BLB.Tagger.HyperLinks = 'hover'; // 'all', 'none', 'hover'
-        BLB.Tagger.HideTranslationAbbrev = true;
-        BLB.Tagger.TargetNewWindow = true;
-        BLB.Tagger.Style = 'line'; // 'line' or 'par'
-        BLB.Tagger.NoSearchTagNames = 'h1,h2'; // HTML element list
-        BLB.Tagger.NoSearchClassNames = 'noTag doNotTag sidebar'; // CSS class list
 
-        // Re-init with the new settings
-        BLB.Tagger.init();
-        console.log('✅ BLB Tagger re-initialised with custom settings.');
-    } else {
-        // Try again in 200ms until BLB is available
-        setTimeout(initBLBTagger, 200);
-    }
-})();
+const redirects = {
+  "/the-question-about-resurrection-luke.html": "/whose-wife-will-she-be-neville-goddard.html"
+};
+
+const current = window.location.pathname;
+if (redirects[current]) {
+  window.location.replace(redirects[current]);
+}
+
+
+
+
+
+
+
